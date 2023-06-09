04f191a93d0a163d894a185f805369e72c98c639
Sofia-Bourdon <mabourdonsofia@gmail.com>
2023-06-09 14:29:01 +0000

    Debugging deployment errors

--- codestar/settings.py
+++ codestar/settings.py
@@ -26,13 +26,15 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0my1secret2key3dj@ngo4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-site.herokuapp.com', 'localhost',]
-
+ALLOWED_HOSTS = [
+    '8000-sofiabourdon-djangoblog-qvgv7gkfi4n.ws-eu99.gitpod.io',
+    'django-blog-site.herokuapp.com', 'localhost',
+    ]
 
 
 # Application definition
@@ -133,7 +135,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path..join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
